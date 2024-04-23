b2bb7abbe263e3161f3a96421374567773acc3fe
ljining <108book@naver.com>
2024-04-23 18:21:43 +0900

    [Add] UI구현

---
+++ Mega6Box/Mega6Box/Controller/MainMovieListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainMovieListViewController.swift
+//  Mega6Box
+//
+//  Created by 이유진 on 4/23/24.
+//
+
+import UIKit
+
+class MainMovieListViewController: UIViewController {
+    
+    @IBOutlet weak var MovieImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+}
--- Mega6Box/Mega6Box/Model/Movie.swift
+++ Mega6Box/Mega6Box/Model/Movie.swift
@@ -7,20 +7,22 @@
 
 import Foundation
 
-struct Movie: Decodable {
-    let originalTitle: String
-    let title: String
-    let overview: String
-    let releaseDate: String
-    let posterPath: String
-    let genreIds: [Int]
+struct MovieResults: Decodable {
+  let results: [Movie]
+}
 
-    enum CodingKeys: String, CodingKey {
-        case originalTitle = "original_title"
-        case title         
-        case overview
-        case releaseDate = "release_date"
-        case posterPath = "poster_path"
-        case genreIds = "genre_ids"
-    }
+struct Movie: Decodable {
+  let id: Int
+  let title: String
+  let overview: String
+  let backdropPath: String
+  let releaseDate: String
+    
+  enum CodingKeys: String, CodingKey {
+    case id
+    case title
+    case overview
+    case backdropPath = "backdrop_path"
+    case releaseDate = "release_date"
+  }
 }
