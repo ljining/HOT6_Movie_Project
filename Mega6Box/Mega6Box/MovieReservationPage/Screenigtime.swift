ba93388f8a5298af2bb26c86d29d769746dcf248
Hee48 <lvovl7070@naver.com>
2024-04-25 10:09:40 +0900

    컬렉션뷰수정부분

--- Mega6Box/Mega6Box/MovieReservationPage/Screenigtime.swift
+++ Mega6Box/Mega6Box/MovieReservationPage/Screenigtime.swift
@@ -20,3 +20,55 @@ extension Screenigtime {
     ]
 }
 
+//컬렉션뷰 셀잡히게 하려고 테스트데이터
+struct Day {
+    let date: String
+}
+
+extension Day {
+    static var data = [
+    Day(date: "22\n오늘"),
+    Day(date: "23\n수"),
+    Day(date: "24\n목"),
+    Day(date: "25\n금"),
+    Day(date: "26\n토"),
+    ]
+    //엔티티 > 속성 > 앱델리게잇에서 스택을 구현하고 > 컨텐트를 만들어서(사용할때만-인스턴스로만들어짐) > 컨텐트안에 내용을 넣어야함 >
+}
+
+
+
+//struct Date {
+//    let year: Int
+//    let month: Int
+//    let day: Int
+//    
+//    init(year: Int, month: Int, day: Int) {
+//        self.year = year
+//        self.month = month
+//        self.day = day
+//    }
+//}
+//
+//struct Day {
+//    let date: Date
+//    
+//    init(date: Date) {
+//        self.date = date
+//    }
+//}
+//
+//extension Day {
+//    static var days: [Day] {
+//        var days: [Day] = []
+//        
+//        for day in 22...26 {
+//            let date = Date(year: 2024, month: 4, day: day)
+//            days.append(Day(date: date))
+//        }
+//        
+//        return days
+//    }
+//}
+
+
