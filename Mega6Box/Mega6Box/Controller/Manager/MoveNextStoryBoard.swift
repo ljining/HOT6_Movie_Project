18aa19363a903e0e1f5939cf6b0a8069e9e6ed4b
kimtaedam <54749691+incoverlet@users.noreply.github.com>
2024-04-26 14:35:14 +0900

    id 전달값 추가

--- Mega6Box/Mega6Box/Controller/Manager/MoveNextStoryBoard.swift
+++ Mega6Box/Mega6Box/Controller/Manager/MoveNextStoryBoard.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class MoveNextStoryBoard{
     static let shared = MoveNextStoryBoard()
+    //전달하는 무비아이디
+    static var movieID = 0
     
     //ex)     MoveNextStoryBoard.shared.moveNext(storyboardName: "MovieDetail", viewControllerIdentifier: "MovieDetailViewController", from: self)
     func moveNext(storyboardName: String, viewControllerIdentifier: String, from viewController: UIViewController) {
         let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
         if let nextViewController = storyboard.instantiateViewController(withIdentifier: viewControllerIdentifier) as? UIViewController {
+            viewController.navigationController?.pushViewController(nextViewController, animated: true)
+        } else {
+            print("Error: ViewController could not be instantiated or cast to UIViewController")
+        }
+    }
+    
+    func moveNext(storyboardName: String, viewControllerIdentifier: String, from viewController: UIViewController, movieID: Int) {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        if let nextViewController = storyboard.instantiateViewController(withIdentifier: viewControllerIdentifier) as? UIViewController {
+            MoveNextStoryBoard.movieID = movieID
             viewController.navigationController?.pushViewController(nextViewController, animated: true)
         } else {
             print("Error: ViewController could not be instantiated or cast to UIViewController")
