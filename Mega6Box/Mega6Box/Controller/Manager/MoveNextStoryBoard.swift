8ac1a5972dcc559d476fffa4bc0769644d60aae4
kimtaedam <54749691+incoverlet@users.noreply.github.com>
2024-04-26 14:18:33 +0900

    moveNextController

---
+++ Mega6Box/Mega6Box/Controller/Manager/MoveNextStoryBoard.swift
@@ -0,0 +1,24 @@
+//
+//  MoveNextStoryBoard.swift
+//  Mega6Box
+//
+//  Created by 김태담 on 4/26/24.
+//
+
+import Foundation
+import UIKit
+
+class MoveNextStoryBoard{
+    static let shared = MoveNextStoryBoard()
+    
+    //ex)     MoveNextStoryBoard.shared.moveNext(storyboardName: "MovieDetail", viewControllerIdentifier: "MovieDetailViewController", from: self)
+    func moveNext(storyboardName: String, viewControllerIdentifier: String, from viewController: UIViewController) {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        if let nextViewController = storyboard.instantiateViewController(withIdentifier: viewControllerIdentifier) as? UIViewController {
+            viewController.navigationController?.pushViewController(nextViewController, animated: true)
+        } else {
+            print("Error: ViewController could not be instantiated or cast to UIViewController")
+        }
+    }
+    
+}
